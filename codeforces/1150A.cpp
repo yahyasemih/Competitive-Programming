900a87504a5ac26e2fe1f14ce2610a1a311a9f07
Yahya Ez-zainabi <yezzainabi@gmail.com>
2019-10-14 16:26:51 +0100

    Create 1150A.cpp

---
+++ codeforces/1150A.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
+#include <streambuf>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+ 
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+ 
+#define sz(v) ((int)((v).size()))
+#define all(v) ((v).begin(), (v).end())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a;i < b ; i++)
+#define nl << "\n"
+ 
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,m,r, b, s, min = 10001, max = 0;
+    cin >> n >> m >> r;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> s;
+        if (s < min)
+            min = s;
+    }
+     for(int i = 0; i < m; i++)
+    {
+        cin >> b;
+        if (b > max)
+            max = b;
+    }
+    if (min > max)
+        cout << r << endl;
+    else
+        cout << (r/min) * max + r%min << endl;
+	return 0;
+}
