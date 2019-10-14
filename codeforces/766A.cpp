41a4dba6dedd1a03373a5c903a7f7ab149a60df2
Yahya Ez-zainabi <yezzainabi@gmail.com>
2019-10-14 17:10:53 +0100

    Create 766A.cpp

---
+++ codeforces/766A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+ 
+#define sz(v) ((int)((v).size()))
+#define all(v) ((v).begin(), (v).end())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a;i < b ; i++)
+#define nl << "\n"
+ 
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string s1, s2;
+	cin >> s1;
+	cin >> s2;
+	if (s1 == s2)
+		cout << -1 nl;
+	else
+		cout << max(s1.size(), s2.size()) nl;
+	return 0;
+}
