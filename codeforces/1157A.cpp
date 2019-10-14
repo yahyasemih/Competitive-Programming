1e4112fc314aca0d4f573a1429942632a6c3369e
Yahya Ez-zainabi <yezzainabi@gmail.com>
2019-10-14 16:20:25 +0100

    Create 1157A.cpp

---
+++ codeforces/1157A.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
+#include <streambuf>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+ 
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+ 
+#define sz(v) ((int)((v).size()))
+#define all(v) ((v).begin(), (v).end())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a;i < b ; i++)
+#define nl << "\n"
+ull next(ull N)
+{
+	N++;
+	while (N%10==0)
+		N/= 10;
+	return N;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ull N;
+	set<ull> s;
+	cin >> N;
+	s.insert(N);
+	while (s.find(next(N)) == s.end())
+	{
+		N = next(N);
+		s.insert(N);
+	}
+	cout << s.size() << endl;
+	return 0;
+}
