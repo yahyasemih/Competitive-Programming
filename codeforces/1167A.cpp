106555f1534d65ae0beb4b2eb02a3b34a8d0ed23
Yahya Ez-zainabi <yezzainabi@gmail.com>
2019-10-14 16:34:33 +0100

    Create 1167A.cpp

---
+++ codeforces/1167A.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
+#include <streambuf>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+ 
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+ 
+#define sz(v) ((int)((v).size()))
+#define all(v) ((v).begin(), (v).end())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a;i < b ; i++)
+#define nl << "\n"
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		string s;
+		int n;
+		cin >> n >> s;
+		if (s.find('8') != string::npos and s.size() - s.find('8') >= 11)
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+	return 0;
+}
